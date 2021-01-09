18473c9087f6a8ab7c43a4a66464e20b05ba3d55
Seung Hun Jang <sj43@rice.edu>
2021-01-09 21:11:15 +0900

    fixed latest 25 messages error

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function ChatRoom() {
 
   const dummy = useRef();
   const messagesRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
+  const query = messagesRef.orderBy('createdAt', "asc").limitToLast(25);
   const [messages] = useCollectionData(query, { idField: 'id' });
 
   const [formValue, setFormValue] = useState('');
